ffd69e68350ca38d9e68b42f9df3e6884cee7585
root <bit-dev-bot[bot]@users.noreply.github.com>
2020-11-15 14:42:08 +0000

    chore(deps): update bit component cap-test.playground/bit-test-2

--- src/components/bit-test-2/bit-test-2.jsx
+++ src/components/bit-test-2/bit-test-2.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const BitTest2 = () => {
-  return <h2>I am component 2</h2>;
+  return (
+    <div>
+      <h2>I am component 2</h2>
+      <h3>I have changed from consumer</h3>
+    </div>
+  );
 };
 
 export default BitTest2;
